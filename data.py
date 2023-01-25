d33bd303fc9c02c5d3ebe2cb2c24dcce075fe4f3
Harikrishna Pillai <amenu4aie21034@am.students.amrita.edu>
2023-01-25 21:41:13 +0530

    fix: normalized input images to get right output

--- data.py
+++ data.py
@@ -2,6 +2,12 @@ import pyautogui
 import keyboard
 import pyscreenshot as ImageGrab
 from deepDino import run_model
+import cv2
+import numpy as np
+import json
+import onnx
+import onnxruntime as ort
+
 i=0
 is_exit = False
 def exit():
@@ -9,21 +15,53 @@ def exit():
     is_exit = True
 
 keyboard.add_hotkey('esc', exit)
-while (True):
-    if is_exit:
-        break
-    if keyboard.is_pressed(keyboard.KEY_UP):
+def data_collection():
+    while (True):
+        if is_exit:
+            break
+        if keyboard.is_pressed(keyboard.KEY_UP):
 
-        ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
-        ss.save(f'./data/screenshot{i}'+ 'UP'+'.png')
-        i+=1
+            ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
+            ss.save(f'./data/screenshot{i}'+ 'UP'+'.png')
+            i+=1
 
-    if keyboard.is_pressed(keyboard.KEY_DOWN):
-        ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
-        ss.save(f'./data/screenshot{i}'+ 'DOWN'+'.png')
-        i+=1
+        if keyboard.is_pressed(keyboard.KEY_DOWN):
+            ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
+            ss.save(f'./data/screenshot{i}'+ 'DOWN'+'.png')
+            i+=1
 
-    if keyboard.is_pressed('right'):
+        if keyboard.is_pressed('right'):
+            ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
+            ss.save(f'./data/screenshot{i}'+ 'RIGHT'+'.png')
+            i+=1
+
+def AIDino():
+    deepDino = ort.InferenceSession('deepDino.onnx')
+    while (True):
+        if is_exit:
+            break
         ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
-        ss.save(f'./data/screenshot{i}'+ 'RIGHT'+'.png')
+        # ss.save(f'./data/screenshot{i}'+ 'UP'+'.png')
         i+=1
+        # img = cv2.imread('./data/screenshot170UP.png')
+        img = np.array(ss)
+        img = np.dot(img[...,:3], [0.299, 0.587, 0.114])
+        img = cv2.resize(img, dsize=(100, 100), interpolation=cv2.INTER_AREA)
+        # cv2.imwrite('greyscale.png', img)
+        img.resize((1, 1, 100, 100))
+        img = img/255
+        data = json.dumps({'data': img.tolist()})
+        data = np.array(json.loads(data)['data']).astype('float32')
+        pred,actual = run_model(deepDino, data, 0,["UP", "DOWN", "RIGHT"])
+        if pred == "UP":
+            # pyautogui.press('up')
+            keyboard.press_and_release('up')
+        if pred == "DOWN":
+            # pyautogui.press('down')
+            keyboard.press_and_release('down')
+        if pred == "RIGHT":
+            # pyautogui.press('right')
+            keyboard.press_and_release('right')
+
+if __name__ == "__main__":
+    AIDino()
--- deepDino.py
+++ deepDino.py
@@ -10,7 +10,7 @@ onnx.checker.check_model(onnx_model)
 # print(onnx.helper.printable_graph(onnx_model.graph))
 
 # x, y = test_data[0][0], test_data[0][1]
-ort_sess = ort.InferenceSession('deepDino.onnx')
+deepDino = ort.InferenceSession('deepDino.onnx')
 def run_model(model, x,y,classes):
 
     outputs = model.run(None, {'image': x})
@@ -21,18 +21,14 @@ def run_model(model, x,y,classes):
     print(f'Predicted: "{predicted}", Actual: "{actual}"')
 
     return predicted, actual
-# x = Image.open('./data/screenshot170UP.png')
-# x = x.resize((100,100))
-# x = np.array(x)
-# x = x.astype(np.float32)
-# x = x.reshape(1,3,100,100)
 
-img = cv2.imread('./data/screenshot170UP.png')
+
+img = cv2.imread('./data/screenshot4UP.png')
+# print()
 img = np.dot(img[...,:3], [0.299, 0.587, 0.114])
 img = cv2.resize(img, dsize=(100, 100), interpolation=cv2.INTER_AREA)
-cv2.imwrite('greyscale.png', img)
+img = img/255
 img.resize((1, 1, 100, 100))
-
 data = json.dumps({'data': img.tolist()})
 data = np.array(json.loads(data)['data']).astype('float32')
-pred,actual = run_model(ort_sess, data, 0,["UP", "DOWN", "RIGHT"])
+pred,actual = run_model(deepDino, data, 0,["UP", "DOWN", "RIGHT"])
