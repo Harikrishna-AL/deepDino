f7139d5669681cbbaf8901e2eae934d352156d85
Harikrishna-AL <amenu4aie20134@am.students.amrita.edu>
2023-01-29 15:08:45 +0530

    fix: model to get the dino jump

--- data.py
+++ data.py
@@ -1,11 +1,8 @@
-# import pyautogui
+
 import keyboard
 import pyscreenshot as ImageGrab
 from deepDino import run_model
-import cv2
 import numpy as np
-import json
-import onnx
 import onnxruntime as ort
 
 i=0
@@ -16,47 +13,55 @@ def exit():
 
 keyboard.add_hotkey('esc', exit)
 def data_collection():
+    global i
     while (True):
         if is_exit:
             break
         if keyboard.is_pressed(keyboard.KEY_UP):
 
-            ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
-            ss.save(f'./data/screenshot{i}'+ 'UP'+'.png')
+            ss = ImageGrab.grab(bbox=(660, 275, 785, 380))
+            ss.save(f'./new_data/screenshot{i}'+ 'UP'+'.png')
             i+=1
 
         if keyboard.is_pressed(keyboard.KEY_DOWN):
-            ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
-            ss.save(f'./data/screenshot{i}'+ 'DOWN'+'.png')
+            ss = ImageGrab.grab(bbox=(660, 275, 785, 380))
+            ss.save(f'./new_data/screenshot{i}'+ 'DOWN'+'.png')
             i+=1
 
         if keyboard.is_pressed('right'):
-            ss = ImageGrab.grab(bbox=(625, 275, 775, 380))
-            ss.save(f'./data/screenshot{i}'+ 'RIGHT'+'.png')
+            ss = ImageGrab.grab(bbox=(660, 275, 785, 380))
+            ss.save(f'./new_data/screenshot{i}'+ 'RIGHT'+'.png')
             i+=1
 
 def AIDino():
     deepDino = ort.InferenceSession('deepDino.onnx')
-    for i in range(1):
-    # while(1):
+    while(1):
         if is_exit:
             break
-        ss = ImageGrab.grab(bbox=(725, 250, 875, 355))
-        ss.save('screenshot.png')
-        img = np.array(ss)
-        # img = np.dot(img[...,:3], [0.299, 0.587, 0.114])
-        img = cv2.resize(img, dsize=(100, 100), interpolation=cv2.INTER_AREA)
-        img.resize((1, 1, 100, 100))
-        img = img/255
-        data = json.dumps({'data': img.tolist()})
-        data = np.array(json.loads(data)['data']).astype('float32')
-        pred,actual = run_model(deepDino, data, 0,["UP", "DOWN", "RIGHT"])
+        ss = ImageGrab.grab(bbox=(660, 275, 785, 380))
+        ss = ss.convert('L')
+        ss = ss.resize((100, 100))
+        data = np.array(ss.getdata()).reshape(1,ss.size[0], ss.size[1])
+        data = data/255
+        data = data.reshape((1,1,100,100))
+        data = data.astype(np.float32)
+        pred = run_model(deepDino, data, 0,["UP","RIGHT"])
         if pred == "UP":
             keyboard.press_and_release('up')
-        if pred == "DOWN":
-            keyboard.press_and_release('down')
         if pred == "RIGHT":
             keyboard.press_and_release('right')
 
+def test():
+    global i
+    # while(1):
+    for i in range(50):
+        i+=1
+        if is_exit:
+            break
+        ss = ImageGrab.grab(bbox=(660, 275, 785, 380))
+        ss.save(f'screenshot{i}.png')
+
 if __name__ == "__main__":
     AIDino()
+    # test()
+    # data_collection()
--- deepDino.py
+++ deepDino.py
@@ -1,39 +1,8 @@
-import onnx
-import onnxruntime as ort
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-import cv2
-import json
-onnx_model = onnx.load("deepDino.onnx")
-onnx.checker.check_model(onnx_model)
-# print(onnx.helper.printable_graph(onnx_model.graph))
 
-# x, y = test_data[0][0], test_data[0][1]
-deepDino = ort.InferenceSession('deepDino.onnx')
 def run_model(model, x,y,classes):
 
     outputs = model.run(None, {'image': x})
+    predicted = classes[outputs[0][0].argmax(0)]
+    print(f'Predicted: "{predicted}"')
+    return predicted
 
-    # Print Result 
-    print(outputs[0][0])
-    predicted, actual = classes[outputs[0][0].argmax(0)], classes[y]
-    print(f'Predicted: "{predicted}", Actual: "{actual}"')
-
-    return predicted, actual
-
-
-# img = cv2.imread('./data/screenshot147RIGHT.png')
-# # print()
-# # img = np.dot(img[...,:3], [0.299, 0.587, 0.114])
-# img = cv2.resize(img, dsize=(100, 100), interpolation=cv2.INTER_AREA)
-
-# img = img/255
-# # cv2.imshow('image',img)
-# # cv2.waitKey(0)
-# # cv2.imwrite('image.png',img)
-# img.resize((1, 1, 100, 100))
-# # print(img)
-# data = json.dumps({'data': img.tolist()})
-# data = np.array(json.loads(data)['data']).astype('float32')
-# pred,actual = run_model(deepDino, data, 0,["UP", "DOWN", "RIGHT"])
